90910db8ec686ca585c95b8b4e1314e8b1d98800
DannyAhmedApex <Danialapexconsultant@gmail.com>
2023-01-23 14:26:00 -0500

    in order text should be bold

--- app/src/main/java/com/unfinished/dsnp_wallet_kotlin/ui/onboarding/seedphrase/SeedPhraseFragment.kt
+++ app/src/main/java/com/unfinished/dsnp_wallet_kotlin/ui/onboarding/seedphrase/SeedPhraseFragment.kt
@@ -1,17 +1,22 @@
 package com.unfinished.dsnp_wallet_kotlin.ui.onboarding.seedphrase
 
 import android.os.Bundle
+import android.text.method.LinkMovementMethod
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.content.ContextCompat
+import androidx.core.content.res.ResourcesCompat
 import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.GridLayoutManager
 import com.unfinished.dsnp_wallet_kotlin.R
 import com.unfinished.dsnp_wallet_kotlin.data.model.MnemonicWord
 import com.unfinished.dsnp_wallet_kotlin.databinding.FragmentSeedPhraseBinding
 import com.unfinished.dsnp_wallet_kotlin.ui.onboarding.seedphrase.adapter.SeedPhraseAdapter
+import com.unfinished.dsnp_wallet_kotlin.util.createSpannable
 import com.unfinished.dsnp_wallet_kotlin.util.setOnSafeClickListener
+import com.unfinished.dsnp_wallet_kotlin.util.showBrowser
 
 class SeedPhraseFragment : Fragment() {
 
@@ -41,6 +46,17 @@ class SeedPhraseFragment : Fragment() {
             MnemonicWord(id = "11", content = "Running", indexDisplay = "",false),
             MnemonicWord(id = "12", content = "Spin", indexDisplay = "",false),
         )
+        binding.subTitle.apply {
+            linksClickable = false
+            isClickable = false
+            movementMethod = LinkMovementMethod.getInstance()
+            text = createSpannable(
+                content = getString(R.string.seed_phrase_sub_title),
+                typeface = ResourcesCompat.getFont(requireContext(),R.font.poppins_extrabold),
+                highlightTextColor = ContextCompat.getColor(requireContext(),R.color.orange))  {
+                clickable(getString(R.string.seed_phrase_sub_title_highlight)){}
+            }
+        }
         binding.seedPhraseRv.apply {
             layoutManager = GridLayoutManager(requireContext(),2)
             adapter = SeedPhraseAdapter(words)
@@ -61,5 +77,7 @@ class SeedPhraseFragment : Fragment() {
             notifySeedPhraseFragment.show(childFragmentManager,"notify_seed_phrase")
         }
     }
+
+
 
 }
