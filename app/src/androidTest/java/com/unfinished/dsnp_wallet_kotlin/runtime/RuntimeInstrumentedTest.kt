68f722b347773ba08678e561af1c1af9cb641cc5
DannyAhmedApex <Danialapexconsultant@gmail.com>
2023-03-31 16:13:42 -0400

    Added more test cases to test rpc calls and extrinsics

--- app/src/androidTest/java/com/unfinished/dsnp_wallet_kotlin/runtime/RuntimeInstrumentedTest.kt
+++ app/src/androidTest/java/com/unfinished/dsnp_wallet_kotlin/runtime/RuntimeInstrumentedTest.kt
@@ -6,31 +6,35 @@ import androidx.test.ext.junit.runners.AndroidJUnit4
 import com.google.gson.Gson
 import com.unfinished.feature_account.domain.model.planksFromAmount
 import io.novafoundation.nova.common.data.mappers.mapCryptoTypeToEncryption
-import io.novafoundation.nova.common.data.network.runtime.calls.NextAccountIndexRequest
-import io.novafoundation.nova.common.data.network.runtime.calls.transferCall
+import io.novafoundation.nova.common.data.network.runtime.binding.bindAccountInfo
+import io.novafoundation.nova.common.data.network.runtime.calls.*
+import io.novafoundation.nova.common.data.network.runtime.model.SignedBlock
 import io.novafoundation.nova.common.di.FeatureUtils
-import io.novafoundation.nova.common.utils.default
 import io.novafoundation.nova.common.utils.deriveSeed32
 import io.novafoundation.nova.common.utils.orZero
+import io.novafoundation.nova.common.utils.system
 import io.novafoundation.nova.core.model.CryptoType
 import io.novafoundation.nova.runtime.ext.accountIdOf
+import io.novafoundation.nova.runtime.ext.hexAccountIdOf
 import io.novafoundation.nova.runtime.ext.utilityAsset
 import io.novafoundation.nova.runtime.extrinsic.CustomSignedExtensions
 import io.novafoundation.nova.runtime.multiNetwork.chain.model.Chain
+import io.novafoundation.nova.runtime.multiNetwork.getRuntime
+import io.novafoundation.nova.runtime.multiNetwork.getSocket
 import jp.co.soramitsu.fearless_utils.encrypt.EncryptionType
 import jp.co.soramitsu.fearless_utils.encrypt.MultiChainEncryption
-import jp.co.soramitsu.fearless_utils.encrypt.junction.BIP32JunctionDecoder
 import jp.co.soramitsu.fearless_utils.encrypt.junction.SubstrateJunctionDecoder
 import jp.co.soramitsu.fearless_utils.encrypt.keypair.Keypair
-import jp.co.soramitsu.fearless_utils.encrypt.keypair.ethereum.EthereumKeypairFactory
 import jp.co.soramitsu.fearless_utils.encrypt.keypair.substrate.SubstrateKeypairFactory
-import jp.co.soramitsu.fearless_utils.encrypt.seed.ethereum.EthereumSeedFactory
 import jp.co.soramitsu.fearless_utils.encrypt.seed.substrate.SubstrateSeedFactory
 import jp.co.soramitsu.fearless_utils.extensions.fromHex
 import jp.co.soramitsu.fearless_utils.runtime.RuntimeSnapshot
 import jp.co.soramitsu.fearless_utils.runtime.definitions.types.generics.Era
 import jp.co.soramitsu.fearless_utils.runtime.extrinsic.ExtrinsicBuilder
 import jp.co.soramitsu.fearless_utils.runtime.extrinsic.signer.KeyPairSigner
+import jp.co.soramitsu.fearless_utils.runtime.metadata.GetMetadataRequest
+import jp.co.soramitsu.fearless_utils.runtime.metadata.storage
+import jp.co.soramitsu.fearless_utils.runtime.metadata.storageKey
 import jp.co.soramitsu.fearless_utils.wsrpc.executeAsync
 import jp.co.soramitsu.fearless_utils.wsrpc.mappers.ResponseMapper
 import jp.co.soramitsu.fearless_utils.wsrpc.mappers.nonNull
@@ -39,15 +43,14 @@ import jp.co.soramitsu.fearless_utils.wsrpc.request.runtime.RuntimeRequest
 import jp.co.soramitsu.fearless_utils.wsrpc.request.runtime.author.SubmitAndWatchExtrinsicRequest
 import jp.co.soramitsu.fearless_utils.wsrpc.request.runtime.chain.RuntimeVersion
 import jp.co.soramitsu.fearless_utils.wsrpc.request.runtime.chain.RuntimeVersionRequest
-import jp.co.soramitsu.fearless_utils.wsrpc.response.RpcResponse
+import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.runBlocking
+import kotlinx.coroutines.withContext
 import org.junit.Assert
 import org.junit.Test
 import org.junit.runner.RunWith
 import org.junit.Assert.*
 import org.junit.Before
-import java.math.BigDecimal
-import java.math.BigInteger
 
 @RunWith(AndroidJUnit4::class)
 class RuntimeInstrumentedTest {
@@ -62,7 +65,7 @@ class RuntimeInstrumentedTest {
     //Custom details about account
     lateinit var test_account_keypair: Keypair
     lateinit var test2_account_keypair: Keypair
-    val genesisHash = "0xce5d4fe1b183df0434514ebc38f28f1227df817d74b48a979cf5d2cf8e86b7d7"
+    val genesisHash = "0xce5d4fe1b183df0434514ebc38f28f1227df817d74b48a979cf5d2cf8e86b7d7" //don't change it
     var test_account_mnemonic =
         "mouse humble two verify ocean more giant nerve slot joke food forest"
     val test2_account_mnemonic =
@@ -95,25 +98,111 @@ class RuntimeInstrumentedTest {
         val account_address_for_nonce = test_account_address
         val request = NextAccountIndexRequest(account_address_for_nonce)
         val response = executeCall(request)
+        println(gson.toJson(response))
         //Make test fail if the error exists
         Assert.assertTrue(response.error?.message ?: "Error", response.error == null)
+    }
+
+    @Test
+    fun test_runtimeMetadata() = runBlocking {
+        val request = GetMetadataRequest
+        val response = executeCallWithMapper(request, pojo<String>().nonNull())
         println(gson.toJson(response))
+        //Make test fail if the error exists
+        Assert.assertTrue("Metadata is empty or not valid", response.isNotEmpty())
     }
 
     @Test
     fun test_runtimeVersion() = runBlocking {
         val request = RuntimeVersionRequest()
         val response = executeCall(request)
+        println(gson.toJson(response))
         //Make test fail if the error exists
         Assert.assertTrue(response.error?.message ?: "Error", response.error == null)
-        println(gson.toJson(response))
     }
 
     @Test
-    fun testTransfer() = runBlocking {
+    fun test_getGenesisHash() = runBlocking {
+        val request = GetBlockHashRequest(0.toBigInteger())
+        val response = executeCallWithMapper(request,pojo<String>().nonNull())
+        println(response)
+        //Make test fail if the error exists
+        Assert.assertTrue("Block hash is empty",response.isNotEmpty())
+    }
+
+    /**
+     * Retrieves the block with given hash
+     * If hash is null, than the latest block is returned
+     */
+    @Test
+    fun test_getBlock() = runBlocking {
+        val hash: String? = null
+        val request = GetBlockRequest(hash)
+        val response = executeCallWithMapper(request,pojo<SignedBlock>().nonNull())
+        println(gson.toJson(response))
+        //Make test fail if the error exists
+        Assert.assertTrue("Parent hash is null",response.block.header.parentHash?.isNotEmpty() ?: false)
+        Assert.assertTrue("Block number is negative",response.block.header.number > -1)
+    }
+
+    @Test
+    fun test_executeGetStorageRequest() = runBlocking(Dispatchers.Default) {
+        val key = runtime.metadata.system().storage("Account")
+            .storageKey(runtime, chain.hexAccountIdOf(test_account_address).fromHex())
+        val request = GetStateRequest(key)
+        val response = executeCall(request)
+        val scale = response.result as? String
+        val accountInfo = bindAccountInfo(scale!!, runtime)
+        println(gson.toJson(accountInfo))
+        Assert.assertTrue("Scale is empty",scale.isNotEmpty())
+    }
+
+    @Test
+    fun test_createMsaId_for_account() = runBlocking {
+        val accountKeypairForMSA = test_account_keypair
+        val accountAddressForMSA = test_account_address
+        val accountIdForMSA = chain.accountIdOf(test_account_address)
+
+        //Rpc Call for getting nonce
+        val request_nonce = NextAccountIndexRequest(accountAddressForMSA)
+        val response_nonce = executeCall(request_nonce)
+        val doubleResult = response_nonce.result as Double
+
+        //Rpc Call for getting Runtime Version
+        val request_runtimeVersion = RuntimeVersionRequest()
+        val response_runtimeVersion = executeCallWithMapper(request_runtimeVersion,mapper = pojo<RuntimeVersion>().nonNull())
+
+        //Extrinsic Builder
+        val signer = KeyPairSigner(accountKeypairForMSA, MultiChainEncryption.Substrate(mapCryptoTypeToEncryption(CryptoType.SR25519)))
+        val extrinsicBuilder = ExtrinsicBuilder(
+            tip = chain.additional?.defaultTip.orZero(),
+            runtime = runtime,
+            nonce = doubleResult.toInt().toBigInteger(),
+            runtimeVersion = response_runtimeVersion,
+            genesisHash = genesisHash.fromHex(),
+            blockHash = genesisHash.fromHex(),
+            era = Era.Immortal,
+            customSignedExtensions = CustomSignedExtensions.extensionsWithValues(runtime),
+            signer = signer,
+            accountId = accountIdForMSA
+        )
+        val extrinsic = extrinsicBuilder
+            .createMsa()
+            .build()
+
+        val request = SubmitAndWatchExtrinsicRequest(extrinsic)
+        val response = executeCall(request)
+        println(gson.toJson(response))
+        //Make test fail if the error exists
+        Assert.assertTrue(response.error?.message ?: "Error", response.error == null)
+    }
+
+    @Test
+    fun test_balance_transfer() = runBlocking {
         //Account detail for transaction
         val senderAccountKeyPair = test_account_keypair
         val senderAccountAddress = test_account_address
+        val recieverAccountAddress = test2_account_address
         val senderAccountId = chain.accountIdOf(test_account_address)
         val recieverAccountId = chain.accountIdOf(test2_account_address)
 
@@ -148,7 +237,19 @@ class RuntimeInstrumentedTest {
         val response = executeCall(request)
         //Make test fail if the error exists
         Assert.assertTrue(response.error?.message ?: "Error", response.error == null)
-        println(gson.toJson(response))
+        println("Transfer Extrinsic Response:" + gson.toJson(response))
+        println("After Transfer Sender Account Details:" + printAccountDetails(senderAccountAddress))
+        println("After Transfer Reciever Account Details:" + printAccountDetails(recieverAccountAddress))
+    }
+
+    private fun printAccountDetails(address: String): String {
+        val key = runtime.metadata.system().storage("Account").storageKey(runtime, chain.hexAccountIdOf(address).fromHex())
+        val request_storage_query = GetStateRequest(key)
+        val response_storage_query = executeCall(request_storage_query)
+        val scale = response_storage_query.result as? String
+        Assert.assertTrue("Scale is empty",scale?.isNotEmpty() ?: false)
+        val accountInfo = bindAccountInfo(scale!!, runtime)
+        return gson.toJson(accountInfo)
     }
 
     private fun createKeypair(mnemonic: String): Keypair {
