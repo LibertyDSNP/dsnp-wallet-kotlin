deeade15ef3120e7215a6d5fa06f6d02df98619a
DannyAhmedApex <Danialapexconsultant@gmail.com>
2023-06-13 11:07:15 -0400

    Terms handle designed

---
+++ feature_account/src/main/java/com/unfinished/feature_account/presentation/handle/terms/TermsHandleViewModel.kt
@@ -0,0 +1,13 @@
+package com.unfinished.feature_account.presentation.handle.terms
+
+import com.unfinished.feature_account.presentation.AccountRouter
+import dagger.assisted.AssistedInject
+
+import io.novafoundation.nova.common.base.BaseViewModel
+
+class TermsHandleViewModel @AssistedInject constructor(
+    private val router: AccountRouter,
+) : BaseViewModel() {
+
+
+}
