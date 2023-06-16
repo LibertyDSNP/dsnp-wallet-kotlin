76b30f62cb3dc92b1490d6b44d1128197f8902bd
jmiecz <13317202+jmiecz@users.noreply.github.com>
2023-06-16 13:14:47 -0700

    Revert "[59] android identity screen"

--- app/src/main/java/com/unfinished/dsnp_wallet_kotlin/ui/home/uimodel/IdentityUiModel.kt
+++
@@ -1,8 +0,0 @@
-package com.unfinished.dsnp_wallet_kotlin.ui.home.uimodel
-
-data class IdentityUiModel(
-    val iconUrl: String?,
-    val username: String,
-    val currentCount: Int,
-    val totalCount: Int
-)
