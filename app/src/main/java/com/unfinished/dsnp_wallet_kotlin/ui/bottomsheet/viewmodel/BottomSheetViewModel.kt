b500ac68f7e2a790db355addaa08faa27e01fe60
DannyAhmedApex <Danialapexconsultant@gmail.com>
2023-07-10 13:39:17 -0400

    Fixed imports of viewmodels.

--- app/src/main/java/com/unfinished/dsnp_wallet_kotlin/ui/bottomsheet/viewmodel/BottomSheetViewModel.kt
+++ app/src/main/java/com/unfinished/dsnp_wallet_kotlin/ui/bottomsheet/viewmodel/BottomSheetViewModel.kt
@@ -1,9 +1,9 @@
 package com.unfinished.dsnp_wallet_kotlin.ui.bottomsheet.viewmodel
 
+import com.unfinished.common.base.BaseViewModel
 import com.unfinished.dsnp_wallet_kotlin.ui.bottomsheet.uimodel.BottomSheetUiModel
 import com.unfinished.uikit.UiState
 import dagger.hilt.android.lifecycle.HiltViewModel
-import io.novafoundation.nova.common.base.BaseViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asStateFlow
 import javax.inject.Inject
--- app/src/main/java/com/unfinished/dsnp_wallet_kotlin/ui/dialog/viewmodel/DialogViewModel.kt
+++ app/src/main/java/com/unfinished/dsnp_wallet_kotlin/ui/dialog/viewmodel/DialogViewModel.kt
@@ -1,10 +1,10 @@
 package com.unfinished.dsnp_wallet_kotlin.ui.dialog.viewmodel
 
+import com.unfinished.common.base.BaseViewModel
 import com.unfinished.dsnp_wallet_kotlin.ui.NavGraph
 import com.unfinished.dsnp_wallet_kotlin.ui.destinations.DirectionDestination
 import com.unfinished.uikit.UiState
 import dagger.hilt.android.lifecycle.HiltViewModel
-import io.novafoundation.nova.common.base.BaseViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asStateFlow
 import javax.inject.Inject
